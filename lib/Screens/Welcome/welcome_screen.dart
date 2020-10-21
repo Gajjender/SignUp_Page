b2ef776c9b0fcbe13fb3226164be155f926c43cd
Gajjender <rathoregajjender2018@gmail.com>
2020-10-21 13:44:10 +0530

    making Welcome Screen

---
+++ lib/Screens/Welcome/welcome_screen.dart
@@ -0,0 +1,12 @@
+import 'dart:html';
+import 'package:flutter/material.dart';
+import 'package:SignUp_Page/Screens/Welcome/components/body.dart';
+
+class WelcomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
